0381e590cbecf973f53afa052c86beeb61dcafbf
Lucas Felix <lucasfelixdev@gmail.com>
2021-04-28 17:22:39 -0300

    feat: Configuração do styled components finalizada

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ["next/babel"],
+  plugins: [
+    ["styled-components", { "ssr": true }],
+    ["module-resolver", {alias: {"@assets": "./src/assets", "@styles": "./src/styles"}}]
+  ]
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import GlobalStyle from '@styles/Global'
+import theme from '@styles/Theme'
+import { ThemeProvider } from 'styled-components'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
